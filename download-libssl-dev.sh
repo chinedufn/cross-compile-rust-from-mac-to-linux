9b101d07da8880099a8a3a8dea113c0d46f567f8
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2021-06-28 15:29:37 -0400

    Update libssl-dev download links

--- download-libssl-dev.sh
+++ download-libssl-dev.sh
@@ -4,6 +4,6 @@ set -e
 
 cd target
 
-curl -O http://ftp.br.debian.org/debian/pool/main/o/openssl/libssl-dev_1.1.1i-2_arm64.deb
-ar p libssl-dev_1.1.1i-2_arm64.deb data.tar.xz | tar xvf -
-rm -rf libssl-dev_1.1.1h-1_amd64.deb
+curl -O http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl-dev_1.0.1t-1+deb8u12_amd64.deb
+ar p libssl-dev_1.0.1t-1+deb8u12_amd64.deb data.tar.xz | tar xvf -
+rm -rf libssl-dev_1.0.1t-1+deb8u12_amd64.deb
