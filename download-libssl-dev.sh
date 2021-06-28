aec9a767234fe72c2d285f4fda9bc2c37c80ad3f
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2021-06-28 15:52:31 -0400

    Fix download link

--- download-libssl-dev.sh
+++ download-libssl-dev.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+mkdir -p target
 cd target
 
-curl -O http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl-dev_1.0.1t-1+deb8u12_amd64.deb
-ar p libssl-dev_1.0.1t-1+deb8u12_amd64.deb data.tar.xz | tar xvf -
-rm -rf libssl-dev_1.0.1t-1+deb8u12_amd64.deb
+# From time to time the download link gets taken down and needs to be replaced with a new one.
+# Here's where the current download link was found:
+#  https://packages.debian.org/buster/amd64/libssl-dev/download
+curl -O http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl-dev_1.1.1d-0+deb10u6_amd64.deb
+ar p libssl-dev_1.1.1d-0+deb10u6_amd64.deb data.tar.xz | tar xvf -
+rm -rf libssl-dev_1.1.1d-0+deb10u6_amd64.deb
