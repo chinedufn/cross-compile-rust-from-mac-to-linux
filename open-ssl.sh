820bad1044dc4afd774dacb3fe3047bbc0b53ecc
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2021-06-28 15:37:40 -0400

    Remove unused file

--- open-ssl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -O http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl-dev_1.1.1h-1_amd64.deb
-ar p libssl-dev_1.1.1h-1_amd64.deb data.tar.xz | tar xvf -
-rm -rf libssl-dev_1.1.1h-1_amd64.deb
